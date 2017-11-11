375af4c5267efded2dbc8b29ac6c01ea66c6d46e
gamerjon <gamerjon@gmail.com>
2017-11-10 19:17:29 -0600

    cleaned up and optimized ps profiles

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,59 @@
-Function hplus{Iex "$((h|ogv -PassThru).commandline)"}
-
+# Creates a $PROFILE if one does not already exist
+if (-Not (Test-Path $PROFILE)) {
+    New-Item $PROFILE –Type File –Force
+}
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+if (-Not (Test-Path $PROFILE)) {
+    New-Item $PROFILE –Type File –Force
+}
+
+# Modules to import
+Import-Module posh-git
+Import-Module posh-docker
+Import-Module oh-my-posh
+Import-Module PSColor
+
+# Set-Theme for oh-myposh module
+Set-Theme paradox
+
+# CLI history exported to GUI grid view
+Function hplus{Iex "$((h|ogv -PassThru).commandline)"}
+
+# Shortcut to system32 folder
+Function CD32 {set-location c:\windows\system32}
+
+# "grep" the formatted output (display strings) as a stream of strings
+function pgrep {
+  $input | out-string -stream | select-string $args
+}
+
+Function Get-IPAddress {
+ (Get-WmiObject -class win32_NetworkAdapterConfiguration `
+   -Filter 'ipenabled = "true"').ipaddress[0]
+} 
+
+Function Get-IPs {
+ ipconfig | Select-String -Pattern "IPv4"
+}
+
+# Alias to set
+Set-Alias d docker
+Set-Alias dm docker-machine
+Set-Alias vi nvim
+Set-Alias vim nvim
+Set-Alias goto Set-Location
+Set-Alias gh get-help
+Set-Alias np c:\windows\notepad.exe
+Set-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
+Set-Alias go cd32
+Set-Alias gia get-ipaddress
+Set-Alias gips get-ips
+
+# Startup items
+Start-SshAgent
--- profile.ps1
+++ profile.ps1
@@ -1,14 +1,14 @@
 # Import module from previous step
-Import-Module -Name posh-git
+#Import-Module -Name posh-git
 
-Start-SshAgent
+#Start-SshAgent
 
 Write-Host ""
 
-$ScriptPath = Split-Path -parent $PSCommandPath
-. "$ScriptPath\Get-ChildItem-Color\Get-ChildItem-Color.ps1"
-Set-Alias ls Get-ChildItem-Color -Option AllScope -Force
-Set-Alias dir Get-ChildItem-Color -option AllScope -Force
+# $ScriptPath = Split-Path -parent $PSCommandPath
+# . "$ScriptPath\Get-ChildItem-Color\Get-ChildItem-Color.ps1"
+# Set-Alias ls Get-ChildItem-Color -Option AllScope -Force
+# Set-Alias dir Get-ChildItem-Color -option AllScope -Force
 
 function Test-Administrator {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
@@ -21,27 +21,37 @@ $psVerMaj= $host.Version.Major
 $psVerMin= $host.Version.Minor
 $curUser= (Get-ChildItem Env:\USERNAME).Value
 $curComp= (Get-ChildItem Env:\COMPUTERNAME).Value
-$isElevated = ""
+$isElevated = "NORMAL "
+$elevatedColor = "Blue"
+$grammar = "a "
+
+if (Test-Administrator) {  # Use different username if elevated
+	$isElevated = "ELEVATED "
+	$elevatedColor = "Red"
+	$grammar = "an "
+}
 
 Write-Host "Greetings, " -NoNewline -foregroundColor $foregroundColor
 Write-Host "$curUser" -NoNewline -foregroundColor DarkYellow
 Write-Host "!" -foregroundColor $foregroundColor
 Write-Host "It is: $($time.ToLongDateString())"
-Write-Host "You're running PowerShell version: $psVerMaj.$psVerMin" -foregroundColor Green
+Write-Host "You're running $grammar" -NoNewline -foregroundColor Green
+Write-Host "$isElevated" -NoNewline -foregroundColor $elevatedColor
+Write-Host "PowerShell $psVerMaj.$psVerMin session" -foregroundColor Green
 Write-Host "Your computer name is: $curComp" -foregroundColor Green
 Write-Host "Happy scripting!"
 
-function Prompt {
+<# function Prompt {
     $realLASTEXITCODE = $LASTEXITCODE
 
     Write-Host
 
     # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+    # $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
 
     if (Test-Administrator) {  # Use different username if elevated
         Write-Host "(Elevated) " -NoNewline -ForegroundColor White
-	$isElevated = "(Elevated)"
+		$isElevated = "(Elevated)"
     }
 
     Write-Host "$curUser@" -NoNewline -ForegroundColor DarkYellow
@@ -69,4 +79,4 @@ function Prompt {
 
     return "> "
 
-}
+} #>
